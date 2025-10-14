b4270c03ff120d52d765632af12d116c6a2b16df
Solifred38 <fred.fromager@gmail.com>
2025-10-14 19:51:27 +0200

    séparation création VM et Provisioning

--- start-cluster.ps1
+++ start-cluster.ps1
@@ -1,4 +1,4 @@
 $env:VAGRANT_VAGRANTFILE="Vagrantfile.multinodes"
 vagrant destroy -f
 vagrant up --no-provision
-vagrant provision --provision-with cluster-k3s,helm,metallb-install,metallb-config
+#vagrant provision --provision-with cluster-k3s,helm,metallb-install,metallb-config
