d01ab4bfe98eb41390d3d201765174d64bcc92fe
Solifred38 <fred.fromager@gmail.com>
2025-10-22 11:16:17 +0200

    services agent windows

---
+++ agentWindows.ps1
@@ -0,0 +1,1 @@
+java -jar shared\agent.jar -url http://192.168.10.200:8080/ -secret 92f0b5ee54ffc2303f2363ac3f388dddd2446214e4415291d0305ee112e9055e -name "agent windows" -webSocket -workDir "C:\Dev\jenkins-agent"
