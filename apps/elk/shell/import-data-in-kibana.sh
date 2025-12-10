d46d3a1e75db69de3833e7dedb96f0cbeafd4173
Solifred38 <fred.fromager@gmail.com>
2025-12-10 12:23:40 +0100

    fix metricbeat

--- apps/elk/shell/import-data-in-kibana.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# import des objets dans kibana
-sudo chmod +x ./set-env-var.sh
-. ./set-env-var.sh
-echo "addresse ip kibana $KIBANA_IP"
-curl -X POST "http://$KIBANA_IP:5601/api/saved_objects/_import?overwrite=true" \
-  -H "kbn-xsrf: true" \
-  -H "Content-Type: multipart/form-data" \
-  -F "file=@../yaml/dashboard.ndjson"
